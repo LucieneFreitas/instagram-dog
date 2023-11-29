6d09c7f3ed1a78b8fc9f893dfd0daa3e955cdfa7
LucieneFreitas <luciene_freitas@yahoo.com.br>
2023-11-29 20:11:31 -0300

    atualização

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,5 @@
-/* eslint-disable no-unused-vars */
+/*eslint no-undef: "error"*/
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
 
-
-// eslint-disable-next-line react/prop-types
-const Titulo = ({cor, texto}) => {
-  return <h1 style={{color: cor}}>{texto}</h1>;
-};
-
-const App = () => {
-  return (
-    <div>
-   <Titulo cor = 'red' texto='Meu titulo1' />
-   <Titulo cor = 'blue' texto='Meu titulo2' />
-  </div>
-  ); 
-};
-
-export default App;
-  
 
